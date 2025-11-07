1ef46588f2a2205c53c1d7fd467f84a906002828
rohityadav142 <rohityadav63949083@gmail.com>
2025-11-07 16:54:28 +0530

    Initial project upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Card from './components/card'
+import Card from './components/Card'
 import { Routes, Route } from "react-router-dom";
 import HiringForm from "./Components/HiringForm";
 const App = () => {
