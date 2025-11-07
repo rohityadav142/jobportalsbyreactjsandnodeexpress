cf82b8ca80b2e6923b10435e4eb7a3602100ea12
rohityadav142 <rohityadav63949083@gmail.com>
2025-11-07 17:05:51 +0530

    Fixed Components path issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Card from './components/Card'
+import Card from './Components/Card'
 import { Routes, Route } from "react-router-dom";
 import HiringForm from "./Components/HiringForm";
 const App = () => {
